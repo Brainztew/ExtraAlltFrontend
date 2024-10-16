3e1f55e905999e7b221bc88a395d3527e2e0cccd
dennis <isacsson_89@hotmail.com>
2024-10-16 08:58:33 +0200

    CleanReact

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Ask A Bot</h1>
+      <h3>A friend when no one else can! </h3>
+    </>
+  )
+}
+
+export default App
